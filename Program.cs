b4635fa36ac71f04d64447cf6267115add3e94e3
Nekai <alessandrobelotti4570@gmail.com>
2022-05-02 20:36:05 +0200

    Organize startup.

--- Program.cs
+++ Program.cs
@@ -4,42 +4,60 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-builder.Services.AddSingleton<WeatherForecastService>();
+namespace AnyDex {
+	public static class Program {
+		public static void Main(string[] args) {
+			WebApplication app;
+			WebApplicationBuilder builder;
+			string connectionString;
 
-var app = builder.Build();
+			builder = WebApplication.CreateBuilder(args);
+			connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+			AddServices(builder, connectionString);
 
-// Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment()) {
-	app.UseMigrationsEndPoint();
-} else {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+			app = builder.Build();
+			ConfigureHttp(app);
+			ConfigureEndpoints(app);
+
+			app.Run();
+		}
+
+		private static void AddServices(WebApplicationBuilder builder, string connectionString) {
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+			builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+			builder.Services.AddRazorPages();
+			builder.Services.AddServerSideBlazor();
+			builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+			builder.Services.AddSingleton<WeatherForecastService>();        // <- To remove
+		}
+
+		private static void ConfigureHttp(WebApplication app) {
+			// Configure the HTTP request pipeline.
+			if(app.Environment.IsDevelopment()) {
+				app.UseMigrationsEndPoint();
+			} else {
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+		}
+
+		private static void ConfigureEndpoints(WebApplication app) {
+			app.UseStaticFiles();
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.MapControllers();
+			app.MapBlazorHub();
+			app.MapFallbackToPage("/_Host");
+		}
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
